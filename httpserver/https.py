fb1f13cbf388e0b6be21744cfb309285512a0dd4
Masato Siraisi <56631381+siraisi368@users.noreply.github.com>
2022-02-19 23:45:44 +0900

    ローカルホスト向けのhttpリクエストを受け付ける簡易的なサーバソフトウェア

---
+++ httpserver/https.py
@@ -0,0 +1,20 @@
+import http.server
+from msilib.schema import Directory;
+import socketserver;
+import json;
+
+with open('./settings./settings.json') as f :
+        jsn = json.load(f);
+
+PORT = (jsn["port"]);
+Directory = "web";
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=Directory, **kwargs)
+
+with socketserver.TCPServer(("",PORT), Handler) as httpd:
+        print("Server Start in port:", PORT);
+        print("このウインドウはソフト終了時まで閉じないでください。");
+        httpd.serve_forever();
+
